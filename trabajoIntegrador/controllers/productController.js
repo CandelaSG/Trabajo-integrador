a15fd379005c24a40dccba3df16652c391ff8252
CandelaSG <csantoguiliano@udesa.edu.ar>
2023-05-26 21:01:04 -0300

    Empecé a hacer el buscador

--- trabajoIntegrador/controllers/productController.js
+++ trabajoIntegrador/controllers/productController.js
@@ -1,4 +1,8 @@
 const data = require('../db/data');
+const db = require('../database/models');
+const op = db.Sequelize.Op;
+const producto = db.Producto;
+
 const productController = {
     show : function (req, res) {
         let id = req.params.id;
@@ -19,12 +23,29 @@ const productController = {
             profile: data.usuario
         })
     },
-    search: function (req, res){
+    search: (req,res)=>{
+        let busqueda = req.query.search;
+        let filtrado = {where: [
+            {title: {[op.like]: "%" + busqueda + "%"}}
+          ]}
+        producto.findAll(filtrado)
+        .then((result)=>{
+            return res.render("search-results", {
+                informacionSearch: data.productos, 
+                comentariosSearch: data.comentarios
+    
+            })
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+    }
+    /* function (req, res){
         return res.render("search-results", {
             informacionSearch: data.productos, 
             comentariosSearch: data.comentarios
 
         })
-    }
+    } */
 };
 module.exports = productController;
